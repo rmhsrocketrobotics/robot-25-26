db650cdcf865e481eb09e5895a5aaf485ff95491
thedrzw <zianrw@gmail.com>
2025-07-16 13:37:30 -0400

    created a roadrunner path (untested)

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,27 +1,51 @@
+// code copied according to https://github.com/rh-robotics/MeepMeep/blob/master/INSTALL.md
+
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 
 import org.rowlandhall.meepmeep.MeepMeep;
 import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
 import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
+    static final double pi = Math.PI;
+
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
+        Pose2d bucketPose = new Pose2d(54, 58, 3*pi/4);
+        Pose2d leftBlockPose = new Pose2d(48, 44, 3*pi/2);
+        Pose2d middleBlockPose = new Pose2d(59, 44, 3*pi/2);
+
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
+                .setConstraints(40, 50, Math.toRadians(180), Math.toRadians(180), 14.5)
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(38, 62, pi/2))
+                        .waitSeconds(1)
+
+                        .lineToSplineHeading(bucketPose)
+                        .addDisplacementMarker(() -> {
+                            //hypothetical code here
+                        })
+                        .waitSeconds(1) //depositing sample
+                        .splineToSplineHeading(leftBlockPose, 3*pi/2)
+                        .waitSeconds(1) //picking up sample
+                        .lineToSplineHeading(bucketPose)
+                        .waitSeconds(1) //depositing sample
+                        .lineToSplineHeading(middleBlockPose)
+                        .waitSeconds(1) //picking up sample
+                        .lineToSplineHeading(bucketPose)
+                        .waitSeconds(1) //depositing sample
+
+                        .lineToSplineHeading(new Pose2d(50, 50, 3*pi/2))
+                        .splineToSplineHeading(new Pose2d(61, 10), 0)
+                        .lineTo(new Vector2d(61, 45))
+                        .splineToConstantHeading(new Vector2d(40, 12), 0)
+                        .lineTo(new Vector2d(25, 12))
+
+                        .waitSeconds(2)
                         .build());
 
 
