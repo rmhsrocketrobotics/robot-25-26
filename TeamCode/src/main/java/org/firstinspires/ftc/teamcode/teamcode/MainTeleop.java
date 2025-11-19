7375decdbd977407e2ca40de7c3cd7e1bfc7916c
thedrzw <zianrw@gmail.com>
2025-11-19 14:58:07 -0500

    added Vision class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/MainTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/MainTeleop.java
@@ -3,12 +3,21 @@ package org.firstinspires.ftc.teamcode.teamcode;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+//import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+//import org.firstinspires.ftc.vision.VisionPortal;
+//import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+//import org.firstinspires.ftc.vision.apriltag.AprilTagLibrary;
+//import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+//
+//import java.util.List;
 
 @TeleOp(group = "!main")
 
@@ -18,8 +27,11 @@ public class MainTeleop extends LinearOpMode{
         Drivetrain drivetrain = new Drivetrain(hardwareMap);
 
         DcMotor intake = hardwareMap.get(DcMotor.class, "intake");
-        DcMotor outtake = hardwareMap.get(DcMotor.class, "outtake");
+        DcMotor outtake1 = hardwareMap.get(DcMotor.class, "outtake1");
+        outtake1.setDirection(DcMotor.Direction.REVERSE);
+        DcMotor outtake2 = hardwareMap.get(DcMotor.class, "outtake2");
         DcMotor flick = hardwareMap.get(DcMotor.class, "flick");
+        CRServo flickServo = hardwareMap.get(CRServo.class, "flickServo");
 
         Gamepad gamepad1Last = new Gamepad();
         Gamepad gamepad2Last = new Gamepad();
@@ -45,9 +57,12 @@ public class MainTeleop extends LinearOpMode{
 
             drivetrain.setDrivetrainPower(-gamepad1.right_stick_y, gamepad1.right_stick_x, gamepad1.left_stick_x);
 
-            outtake.setPower(-gamepad2.left_stick_y);
-            intake.setPower(-gamepad2.right_stick_y);
+            outtake1.setPower(-gamepad2.left_stick_y);
+            outtake2.setPower(-gamepad2.left_stick_y);
+
+            //intake.setPower(-gamepad2.right_stick_y);
             flick.setPower(gamepad2.left_trigger);
+            flickServo.setPower(gamepad2.right_stick_y);
 
             drivetrain.printTelemetry(telemetry);
             telemetry.update();
@@ -104,3 +119,71 @@ class Drivetrain {
         telemetry.addData("brdrivetrain is ", brMotor.getCurrentPosition());
     }
 }
+
+//class Vision {
+//    public void aprilTags(){
+//
+//        List<AprilTagDetection> detections = telemetryAprilTag();
+//        if (gamepad1.dpad_down){
+//            visionPortal.stopStreaming();
+//        } else if (gamepad1.dpad_up){
+//            visionPortal.resumeStreaming();
+//        }
+//        sleep(20);
+//    }
+//
+//        visionPortal.close();
+//    }
+//    public String obelisk(){
+//        List<AprilTagDetection> detections = telemetryAprilTag();
+//        for (AprilTagDetection detection : tagProcessor.getDetections()) {
+//            if (detection.id = 21) {
+//                return "green, purple, purple";
+//            } else if (detection.id = 22) {
+//                return "purple, green, purple";
+//            } else if (detection.id = 23) {
+//                return "purple, purple, green";
+//            } else {
+//                return "no pattern";
+//            }
+//        }
+//    }
+//    public double distance(){
+//        for (AprilTagDetection detection : tagProcessor.getDetections()) {
+//            return detection.robotPose.getPosition().y;
+//        }
+//    }
+//}
+//
+//private void initAprilTag(){
+//    AprilTagMetadata myAprilTagMetadata;
+//    AprilTagLibrary.Builder myAprilTagLibraryBuilder;
+//    AprilTagProcessor.Builder myAprilTagProcessorBuilder;
+//    AprilTagLibrary myAprilTagLibrary;
+//    AprilTagProcessor myAprilTagProcessor;
+//
+//    public myAprilTagLibraryBuilder = new AprilTagLibrary.Builder();
+//    myAprilTagLibraryBuilder.addTag(myAprilTagMetadata);
+//    myAprilTagLibrary = myAprilTagLibraryBuilder.build();
+//    myAprilTagProcessorBuilder = new AprilTagProcessor.Builder();
+//    myAprilTagProcessorBuilder.setTagLibrary(myAprilTagLibrary);
+//    public AprilTagProcessor aprilTag = myAprilTagProcessorBuilder.build();
+//    public VisionPortal.Builder builder = new VisionPortal.Builder();
+//    if (USE_WEBCAM){
+//        builder.setCamera(hardwareMap.get(WebcamName.class, "camera"));
+//    } else {
+//        builder.setCamera(BuiltinCameraDirection.BACK);
+//    }
+//    builder.addProcessor(aprilTag);
+//    public VisionPortal visionPortal = builder.build();
+//}
+//private List<AprilTagDetection> telemetryAprilTag(){
+//    List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+//    return currentDetections.size();
+//    return currentDetections;
+//}   // end method telemetryAprilTag()
+//
+//static double clamp(double value, double min, double max) {
+//    return Math.min(Math.max(min, value), max);
+//} // end class
+
